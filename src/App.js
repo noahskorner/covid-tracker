e799513368487115a03d96cc4d4a4410d5fc2b42
Noah Korner <noahskorner@gmail.com>
2020-08-27 11:01:19 -0700

    Created basic infected, recovered, deaths components. Started work on charts.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import styles from "./App.module.css";
 import { fetchData } from "./api";
 
 class App extends React.Component {
-  async componentDidMount() {
-    const data = await fetchData();
+  state = {
+    data: {},
+  };
 
-    console.log(data);
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
   render() {
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={this.state.data} />
         <CountryPicker />
         <Chart />
       </div>
